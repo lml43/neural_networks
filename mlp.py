5d6b69d1bcdb3458f834348f831b6c9297c2a114
manhlinh0403 <manhlinh0403@gmail.com>
2021-05-26 22:46:21 +0200

    batch and Layer class

--- mlp.py
+++ mlp.py
@@ -1,18 +1,12 @@
 import pandas
 import numpy as np
+import random
 
-learning_rate = 1
 
-I_dim = 2
-H_dim = 4
-
-epoch_count = 2
-
-weights_ItoH = np.random.uniform(-1, 1, (I_dim, H_dim))
-weights_HtoO = np.random.uniform(-1, 1, H_dim)
-
-preActivation_H = np.zeros(H_dim)
-postActivation_H = np.zeros(H_dim)
+class Layer:
+    def __init__(self, activation, size):
+        self.activation = activation
+        self.size = size
 
 
 # Activation functions
@@ -24,55 +18,67 @@ def logistic_deriv(x):
     return logistic(x) * (1 - logistic(x))
 
 
-def print_mat(mat):
-    for rows in mat:
-        for x in rows:
-            print('{:.2f}\t'.format(x), end='')
-        print()
+# Variables
+learning_rate = 1
 
+I_dim = 2
+H_dim = 4
 
-def print_vec(mat):
-    for x in mat:
-        print('{:.2f}\t'.format(x),  end='')
+epoch_count = 100
+batch_size = 16
 
+weights_ItoH = np.random.uniform(-1, 1, (I_dim, H_dim))
+weights_HtoO = np.random.uniform(-1, 1, H_dim)
 
-input_file = 'data/classification/data.simple.test.100.csv'
+preActivation_H = np.zeros(H_dim)
+postActivation_H = np.zeros(H_dim)
+
+input_file = 'data/classification/data.simple.train.10000.csv'
 print("Import file {} ...".format(input_file))
 training_data = pandas.read_csv(input_file)
-target_cls = training_data.cls
+training_class = training_data.cls
 training_data = training_data.drop(['cls'], axis=1)
 training_data = np.asarray(training_data)
-training_count = len(training_data[:,0])
+training_count = len(training_data[:, 0])
 
 #####################
-#training
+# training
 #####################
 for epoch in range(epoch_count):
     print("\n\nEpoch {}".format(epoch))
-    for sample in range(training_count):
-        for node in range(H_dim):
-            preActivation_H[node] = np.dot(training_data[sample,:], weights_ItoH[:, node])
-            postActivation_H[node] = logistic(preActivation_H[node])
 
-        preActivation_O = np.dot(postActivation_H, weights_HtoO)
-        postActivation_O = logistic(preActivation_O)
+    # Shuffle the array
+    random.shuffle(training_data)
 
-        FE = postActivation_O - target_cls[sample]
+    # Iteratively picking up points in a batch
+    for index in range(0, training_count, batch_size):
+        for point in range(index, min(index + batch_size, training_count)):
+            # TODO for each layer
+            for layer_node in range(H_dim):
+                preActivation_H[layer_node] = np.dot(training_data[point, :], weights_ItoH[:, layer_node])
+                postActivation_H[layer_node] = logistic(preActivation_H[layer_node])
 
-        for H_node in range(H_dim):
-            S_error = FE * logistic_deriv(preActivation_O)
-            gradient_HtoO = S_error * postActivation_H[H_node]
-            for I_node in range(I_dim):
-                input_value = training_data[sample, I_node]
-                gradient_ItoH = S_error * weights_HtoO[H_node] * logistic_deriv(preActivation_H[H_node]) * input_value
+            # TODO check classification or regression
+            preActivation_O = np.dot(postActivation_H, weights_HtoO)
+            postActivation_O = logistic(preActivation_O)
 
-                weights_ItoH[I_node, H_node] -= learning_rate * gradient_ItoH
+            error = postActivation_O - training_class[point]
 
-            weights_HtoO[H_node] -= learning_rate * gradient_HtoO
+            # Backpropagation
+            # TODO for each layer
+            for H_node in range(H_dim):
+                S_error = error * logistic_deriv(preActivation_O)
+                gradient_HtoO = S_error * postActivation_H[H_node]
+                for I_node in range(I_dim):
+                    input_value = training_data[point, I_node]
+                    gradient_ItoH = S_error * weights_HtoO[H_node] * logistic_deriv(preActivation_H[H_node]) * input_value
+                    weights_ItoH[I_node, H_node] -= learning_rate * gradient_ItoH
+
+                weights_HtoO[H_node] -= learning_rate * gradient_HtoO
 
     print('I to H:')
-    print_mat(weights_ItoH)
+    print(weights_ItoH)
 
     print()
     print('H to O:')
-    print_vec(weights_HtoO)
+    print(weights_HtoO)
